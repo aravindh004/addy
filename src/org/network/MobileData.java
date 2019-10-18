49db3c80efdbeed5cd85b8bd61fd79a411bf2d9d
addy <v.aravindh2018@gmail.com>
2019-10-18 11:00:21 +0530

    ref

---
+++ src/org/network/MobileData.java
@@ -0,0 +1,8 @@
+package org.network;
+
+public class MobileData extends Lan {
+public void dataName() {
+System.out.println("Data Name");
+}
+
+}
---
+++ src/org/network/Wifi.java
@@ -0,0 +1,16 @@
+package org.network;
+
+public class Wifi extends MobileData{
+	private void wifiName() {
+System.out.println("Wifi Name");
+	}
+	public static void main(String[] args)
+	{
+	Wifi w=new Wifi();
+	w.wifiName();
+	w.dataName();
+	w.lanName();
+	w.modamName();
+	}
+
+}
